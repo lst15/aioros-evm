e4bfa281f5241eaf6ffddd1ef2048f6bdc6cecb0
moinho <leandrosjj5@gmail.com>
2025-03-28 16:10:41 -0300

    Initialize Spring Boot project with Maven wrapper
    Set up a basic Spring Boot application, including Maven wrapper scripts, a POM file with essential dependencies, a `.gitignore` file for common IDEs and tools, and a sample test class. Configured `application.properties` with the default application name.

---
+++ src/main/java/com/aioros/aioros/AiorosApplication.java
@@ -0,0 +1,13 @@
+package com.aioros.aioros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiorosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiorosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aioros/aioros/AiorosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aioros.aioros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiorosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
